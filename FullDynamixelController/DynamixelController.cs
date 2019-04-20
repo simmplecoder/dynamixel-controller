b756097f3c68d968ae36ed8ba618bab8327ac9ca
olzhas <anonymous.from.applecity@gmail.com>
2019-04-21 01:23:19 +0600

    initial commit

---
+++ FullDynamixelController/DynamixelController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dynamixel_sdk;
+
+namespace read_write
+{
+    class DynamixelController: IDisposable
+    {
+        public static readonly int defaultDxlId = 3;
+        public static readonly int defaultBauldRate = 57142;
+        public static readonly string defaultDeviceName = "COM7";
+
+        public const int TORQUE_ENABLE = 1;
+        public const int TORQUE_DISABLE = 0;
+
+        public const int ADDR_MX_TORQUE_ENABLE = 24;                  // Control table address is different in Dynamixel model
+        public const int ADDR_MX_GOAL_POSITION = 30;
+        public const int ADDR_MX_PRESENT_POSITION = 36;
+
+        public const int PROTOCOL_VERSION = 1;                   // See which protocol version is used in the Dynamixel
+
+        public const int COMM_SUCCESS = 0;                   // Communication Success result value
+        public const int COMM_TX_FAIL = -1001;               // Communication Tx Failed
+
+        public const int LB_TORQUE_LIMIT_ADDRESS = 34;
+        public const int HB_TORQUE_LIMIT_ADDRESS = 35;
+
+        private string DeviceName;
+        private int ProtocolVersion;
+        private int DXLID;
+        private int BauldRate;
+        private int portnum;
+
+        public DynamixelController(string deviceName, int protocolVersion, int dxlid, int bauldRate)
+        {
+            DeviceName = deviceName;
+            ProtocolVersion = protocolVersion;
+            DXLID = dxlid;
+            BauldRate = bauldRate;
+
+            portnum = dynamixel.portHandler(deviceName);
+            dynamixel.packetHandler();
+            bool portOpeningSucceeded = dynamixel.openPort(portnum);
+            if (!portOpeningSucceeded)
+            {
+                dynamixel.write1ByteTxRx(portnum, PROTOCOL_VERSION, (byte)DXLID, ADDR_MX_TORQUE_ENABLE, TORQUE_DISABLE);
+                throw new SystemException("Port opening failed");
+            }
+
+            if (!dynamixel.setBaudRate(portnum, BauldRate))
+            {
+                dynamixel.write1ByteTxRx(portnum, PROTOCOL_VERSION, (byte)DXLID, ADDR_MX_TORQUE_ENABLE, TORQUE_DISABLE);
+                throw new SystemException("Failed to set BauldRate");
+            }
+            setCurrentPosition(0);
+
+            // Enable Dynamixel Torque
+            dynamixel.write1ByteTxRx(portnum, PROTOCOL_VERSION, (byte)DXLID, ADDR_MX_TORQUE_ENABLE, TORQUE_ENABLE);
+            checkSuccess();
+        }
+
+        public void setLBTorqueLimit(Uint16 LBtorqueLimit) {
+            dynamixel.write1ByteTxRx(portnum, PROTOCOL_VERSION, (byte)DXLID, LB_TORQUE_LIMIT_ADDRESS, (byte)LBtorqueLimit);
+        }
+
+        public void setHBTorqueLimit(Uin16 HBtorqueLimit) {
+            dynamixel.write1ByteTxRx(portnum, PROTOCOL_VERSION, (byte)DXLID, HB_TORQUE_LIMIT_ADDRESS, (byte)HBtorqueLimit);
+        }
+
+        public void setGoalPosition(UInt16 goalPosition)
+        {
+            dynamixel.write2ByteTxRx(portnum, PROTOCOL_VERSION, (byte)DXLID, ADDR_MX_GOAL_POSITION, goalPosition);
+            checkSuccess();
+        }
+
+        public void setCurrentPosition(Uint16 currentPosition) {
+            dynamxel.write2ByteTxRx(portnum, PROTOCOL_VERSION, (byte)DXLID, ADDR_MX_PRESENT_POSITION, 0);
+        }
+
+        public UInt16 getCurrentPosition()
+        {
+            UInt16 dxl_present_position = dynamixel.read2ByteTxRx(portnum, PROTOCOL_VERSION, (byte)DXLID, ADDR_MX_PRESENT_POSITION);
+            checkSuccess();
+            return dxl_present_position;
+        }
+
+        private void checkSuccess()
+        {
+            int dxl_comm_result = dynamixel.getLastTxRxResult(portnum, PROTOCOL_VERSION);                                   // Communication result
+            byte dxl_error = dynamixel.getLastRxPacketError(portnum, PROTOCOL_VERSION);
+            if (dxl_comm_result != COMM_SUCCESS)
+            {
+                dynamixel.write1ByteTxRx(portnum, PROTOCOL_VERSION, (byte)DXLID, ADDR_MX_TORQUE_ENABLE, TORQUE_DISABLE);
+                throw new SystemException(Marshal.PtrToStringAnsi(dynamixel.getTxRxResult(PROTOCOL_VERSION, dxl_comm_result)));
+            }
+            else if ((dxl_error = dynamixel.getLastRxPacketError(portnum, PROTOCOL_VERSION)) != 0)
+            {
+                dynamixel.write1ByteTxRx(portnum, PROTOCOL_VERSION, (byte)DXLID, ADDR_MX_TORQUE_ENABLE, TORQUE_DISABLE);
+                throw new SystemException(Marshal.PtrToStringAnsi(dynamixel.getRxPacketError(PROTOCOL_VERSION, dxl_error)));
+            }
+        }
+
+        public void Dispose()
+        {
+            dynamixel.write1ByteTxRx(portnum, PROTOCOL_VERSION, (byte)DXLID, ADDR_MX_TORQUE_ENABLE, TORQUE_DISABLE);
+            try
+            {
+                checkSuccess();
+            } catch(Exception) { }
+
+            // Close port
+            dynamixel.closePort(portnum);
+        }
+    }
+}
---
+++ FullDynamixelController/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using dynamixel_sdk;
+
+namespace read_write
+{
+    class ReadWrite
+    {
+
+        public const int DXL_MINIMUM_POSITION_VALUE = 300;                 // Dynamixel will rotate between this value
+        public const int DXL_MAXIMUM_POSITION_VALUE = 4000;                // and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
+        public const int DXL_MOVING_STATUS_THRESHOLD = 10;                  // Dynamixel moving status threshold
+
+        public const byte ESC_ASCII_VALUE = 0x1b;
+
+        public const int COMM_SUCCESS = 0;                   // Communication Success result value
+        public const int COMM_TX_FAIL = -1001;               // Communication Tx Failed
+
+        static void Main(string[] args)
+        {
+            testControllerClass();
+        }
+
+        private static void testControllerClass()
+        {
+            using (var controller = new DynamixelController(DynamixelController.defaultDeviceName,
+                                                            1, DynamixelController.defaultDxlId,
+                                                            DynamixelController.defaultBauldRate))
+            {
+                Console.WriteLine("Dynamixel has been successfully connected");
+                int index = 0;
+                UInt16[] dxl_goal_position = new UInt16[2] { DXL_MINIMUM_POSITION_VALUE, DXL_MAXIMUM_POSITION_VALUE };         // Goal position
+
+                while (true)
+                {
+                    Console.WriteLine("Press any key to continue! (or press ESC to quit!)");
+                    if (Console.ReadKey().KeyChar == ESC_ASCII_VALUE)
+                        break;
+
+                    // Write goal position
+                    controller.setGoalPosition(dxl_goal_position[index]);
+                    UInt16 dxl_present_position = 0;
+                    do
+                    {
+                        // Read present position
+                        dxl_present_position = controller.getCurrentPosition();
+
+                        Console.WriteLine("[ID: {0}] GoalPos: {1}  PresPos: {2}", 3, dxl_goal_position[index], dxl_present_position);
+
+                    } while ((Math.Abs(dxl_goal_position[index] - dxl_present_position) > DXL_MOVING_STATUS_THRESHOLD));
+
+                    // Change goal position
+                    if (index == 0)
+                    {
+                        index = 1;
+                    }
+                    else
+                    {
+                        index = 0;
+                    }
+                }
+            }
+        }
+    }
+}
